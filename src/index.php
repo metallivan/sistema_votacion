efd30d547eb667516ff3849b53640adc523c2885
Iván Hernández Contreras <iva.hernandez.con@gmail.com>
2023-04-27 22:23:29 -0400

    Validaciones Javascript y PHP

--- src/index.php
+++ src/index.php
@@ -26,28 +26,31 @@ function validar_nombre_apellido($nombre) {
     return preg_match('/^[a-zA-ZñÑáéíóúÁÉÍÓÚäëïöüÄËÏÖÜ\s]+$/', $nombre) === 1;
 }
 
-function validar_rut($rut) {
-    $rut = preg_replace('/[^0-9kK]/', '', $rut);
-
-    if (strlen($rut) < 8 || strlen($rut) > 9) {
-        return false;
-    }
-
-    $rut_num = substr($rut, 0, -1);
-    $verif_num = substr($rut, -1);
-
+function validarRut($rut) {
+    $rut = preg_replace('/[^0-9Kk]/', '', $rut); // Eliminar guiones y puntos, dejar solo números y K/k
+    $dv = strtoupper(substr($rut, -1)); // Obtener dígito verificador
+    $num = substr($rut, 0, -1); // Obtener número sin dígito verificador
+  
+    $factores = array(2, 3, 4, 5, 6, 7);
     $suma = 0;
-    $factor = 2;
-    for ($i = strlen($rut_num) - 1; $i >= 0; $i--) {
-        $suma += $factor * $rut_num[$i];
-        $factor = $factor % 7 == 0 ? 2 : $factor + 1;
+    for ($i = strlen($num) - 1, $j = 0; $i >= 0; $i--, $j++) {
+      if ($j >= count($factores)) {
+        $j = 0;
+      }
+      $suma += $num[$i] * $factores[$j];
     }
-
-    $digito_verif = 11 - ($suma % 11);
-    $digito_verif = $digito_verif == 11 ? 0 : ($digito_verif == 10 ? 'K' : $digito_verif);
-
-    return strtoupper($digito_verif) == strtoupper($verif_num);
-}
+    $resto = $suma % 11;
+  
+    if ($dv == '0' && $resto == 0) {
+      return true;
+    } elseif ($dv == 'K' && $resto == 10) {
+      return true;
+    } elseif ($dv == (11 - $resto)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 function validar_email($email) {
     return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
@@ -62,32 +65,26 @@ function validar_coleccion_fuentes($fuentes) {
     return count($fuentes) >= 2 !== false;
 }
 
-// if (validar_email($email) && validar_rut($rut)):
-    // $query = "INSERT INTO votaciones (nombre, alias, rut, email, region_id, comuna_id, candidato_id, fuentes) VALUES (:nombre, :alias, :rut, :email, :region_id, :comuna_id, :candidato_id, :fuentes)";
+if (validar_email($email) && validar_rut($rut)):
+    $query = "INSERT INTO votaciones (nombre, alias, rut, email, region_id, comuna_id, candidato_id, fuentes) VALUES (:nombre, :alias, :rut, :email, :region_id, :comuna_id, :candidato_id, :fuentes)";
     
-    // $stmt = $conn->prepare($query);
-    // $exec = $stmt->execute([
-    //     'nombre' => $nombre, 
-    //     'alias' => $alias,
-    //     'rut' => $rut,
-    //     'email' => $email,
-    //     'region_id' => $region,
-    //     'comuna_id' => $comuna,
-    //     'candidato_id' => $candidato,
-    //     'fuentes' => json_encode($fuentes),
-    // ]);
+    $stmt = $conn->prepare($query);
+    $exec = $stmt->execute([
+        'nombre' => $nombre, 
+        'alias' => $alias,
+        'rut' => $rut,
+        'email' => $email,
+        'region_id' => $region,
+        'comuna_id' => $comuna,
+        'candidato_id' => $candidato,
+        'fuentes' => json_encode($fuentes),
+    ]);
     
-    // if ($exec) {
-    //     echo "Ingresado";
-    // } else {
-    //     echo "NO ingresado";
-    // }
-// endif;
+    if ($exec) {
+        echo "Ingresado";
+    } else {
+        echo "NO ingresado";
+    }
+endif;
 
-
-
-/**
- * REALIZAR FUNCIONES PARA VALIDACIONES DE DATOS E INSERTAR EN LA BASE DE DATOS
- * 
- */
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,181 @@
 'use strict'
 
+const soloLetras = event => {
+    var charCode = event.keyCode;
+    if (charCode > 32 && (charCode < 65 || charCode > 90)
+                      && (charCode < 97 || charCode > 122)
+                      && (charCode < 129 || charCode > 163)
+                      && (charCode < 209 || charCode > 249)) {
+      event.preventDefault();
+      return false;
+    }
+    return true;
+};
+
+const validarNombreApellido = (nombre) => {
+    const regex = /^[a-zA-ZñÑáéíóúÁÉÍÓÚäëïöüÄËÏÖÜ\s]+$/;
+    nombre = nombre.trim();
+    return regex.test(nombre);
+};
+
+const validarRUT = (rut) => {
+
+    if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test(rut)) {
+        return false;
+      }
+      var tmp = rut.split('-');
+      var digv = tmp[1];
+      var rut = tmp[0];
+      if (digv == 'K') digv = 'k';
+      var m = 0, s = 1;
+      for (; rut; rut = Math.floor(rut / 10)) {
+        s = (s + rut % 10 * (9 - m++ % 6)) % 11;
+      }
+      return digv == (s ? s - 1 : 'k');
+};
+
+const validarEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+};
+
+const validarAlias = (alias) => {
+    const regex = /^[a-zA-Z0-9]{6,}$/;
+    alias = alias.trim();
+    return regex.test(alias);
+};
+
+const validarColeccionFuentes = (fuentes) => {
+    return fuentes >= 2 !== false;
+};
+
 $(document).ready(function () {
+
+    $("#nombre").keypress( e => soloLetras(e) );
+    
+    $("#rut").on("input", function() {
+        var regex = /^[0-9kK\-]*$/;
+        if (!regex.test(this.value)) {
+          this.value = this.value.replace(/[^0-9kK\-]/g, "");
+        }
+    });
+
+    $("#alias").on("input", function() {
+        var regex = /^[0-9a-zA-Z]*$/;
+        if (!regex.test(this.value)) {
+          this.value = this.value.replace(/[^0-9a-zA-Z]/g, "");
+        }
+    });
+
+    const validarCampoNombre = () => {
+        let nombre = $("#nombre");
+        nombre.removeClass("is-valid is-invalid");
+
+        $("#alerta-nombre")
+            .removeClass('valid-feedback invalid-feedback')
+            .text("");
+
+        if(validarNombreApellido(nombre.val())) {
+            $("#alerta-nombre")
+                .addClass('valid-feedback')
+                .text("");
+            nombre.addClass('is-valid');
+        } else {
+            let msg = nombre.val() === "" ? "Ingrese su nombre y apellido" : "Nombre incorrecto";
+            $("#alerta-nombre")
+                .addClass('invalid-feedback')
+                .text(msg);
+            nombre.addClass('is-invalid');
+        }
+    };
+
+    const validarCampoRut = () => {
+        let rut = $("#rut");
+        rut.removeClass("is-valid is-invalid");
+
+        $("#alerta-rut")
+            .removeClass('valid-feedback invalid-feedback')
+            .text("");
+
+            if(validarRUT(rut.val())) {
+                $("#alerta-rut")
+                    .addClass('valid-feedback')
+                    .text("");
+                rut.addClass('is-valid');
+            } else {
+                let msg = rut.val() === "" ? "Ingrese su rut" : "Rut no existe. Favor de ingresar correctamente.";
+                $("#alerta-rut")
+                    .addClass('invalid-feedback')
+                    .text(msg);
+                rut.addClass('is-invalid');
+            }
+    };
+
+    const validarCampoAlias = () => {
+        let alias = $("#alias");
+        alias.removeClass("is-valid is-invalid");
+
+        $("#alerta-alias")
+            .removeClass('valid-feedback invalid-feedback')
+            .text("");
+
+            if(validarAlias(alias.val())) {
+                $("#alerta-alias")
+                    .addClass('valid-feedback')
+                    .text("");
+                alias.addClass('is-valid');
+            } else {
+                let msg = alias.val() === "" ? "Ingrese un alias" : "El alias debe contener mas de 5 caracteres";
+                $("#alerta-alias")
+                    .addClass('invalid-feedback')
+                    .text(msg);
+                alias.addClass('is-invalid');
+            }
+    };
+    
+    const validarCampoEmail = () => {
+        let email = $("#email");
+        email.removeClass("is-valid is-invalid");
+
+        $("#alerta-email")
+            .removeClass('valid-feedback invalid-feedback')
+            .text("");
+
+            if(validarEmail(email.val())) {
+                $("#alerta-email")
+                    .addClass('valid-feedback')
+                    .text("");
+                email.addClass('is-valid');
+            } else {
+                let msg = email.val() === "" ? "Ingrese un email" : "El email no es valido. Ingrese nuevamente.";
+                $("#alerta-email")
+                    .addClass('invalid-feedback')
+                    .text(msg);
+                email.addClass('is-invalid');
+            }
+    };
+
+    const validarOpcionesFuentes = () => {
+        let checked = $('input[name="fuentes[]"]:checked').length;
+        let fuentes = $('input[name="fuentes[]"]');
+        fuentes.removeClass("is-invalid is-valid");
+
+        $("#alerta-fuentes")
+            .removeClass('valid-feedback invalid-feedback')
+            .text("");
+
+        if (!validarColeccionFuentes(checked)) {
+            $("#alerta-fuentes")
+                .addClass('invalid-feedback')
+                .text("Debe seleccionar al menos 2 opciones");
+            fuentes.addClass('is-invalid');
+        } else {
+            $("#alerta-fuentes")
+                .addClass('valid-feedback')
+                .text("");
+            fuentes.addClass('is-valid');
+        }
+    };
     
     $.ajax({
         url: 'src/regiones.php',
@@ -50,11 +225,15 @@ $(document).ready(function () {
     });
 
     
-    $("#votarBtn").on('submit', function (e) {
+    $("#votarBtn").on('click', function (e) {
         let formData = $("form").serialize();
         e.preventDefault();
         
-        
+        validarCampoNombre();
+        validarCampoRut();
+        validarCampoAlias();
+        validarCampoEmail();
+        validarOpcionesFuentes();
         $.ajax({
             type: "POST",
             url: "src/index.php",
@@ -69,52 +248,4 @@ $(document).ready(function () {
         });
     });
 
-    /**
-     * REALIZAR VALIDACIONES DE DATOS Y MOSTRAR ALERTAS SEGUN VALIDACIONES
-     */
-
 });
-
-const validarRUT = (rut) => {
-    rut = rut.replace(/[^\dkK]/g, '');
-    
-    if (rut.length < 8 || rut.length > 9) {
-      return false;
-    }
-    
-    const rut_num = rut.slice(0, -1);
-    const verif_num = rut.slice(-1).toUpperCase();
-    
-    let suma = 0;
-    let factor = 2;
-    for (let i = rut_num.length - 1; i >= 0; i--) {
-      suma += factor * parseInt(rut_num[i]);
-      factor = factor % 7 === 0 ? 2 : factor + 1;
-    }
-    
-    let digito_verif = 11 - (suma % 11);
-    digito_verif = digito_verif === 11 ? 0 : (digito_verif === 10 ? 'K' : digito_verif);
-    
-    return digito_verif.toString().toUpperCase() === verif_num;
-};
-
-const validarEmail = (email) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-};
-
-const validarNombreApellido = (nombre) => {
-    const regex = /^[a-zA-ZñÑáéíóúÁÉÍÓÚäëïöüÄËÏÖÜ\s]+$/;
-    nombre = nombre.trim();
-    return regex.test(nombre);
-};
-
-const validarAlias = (alias) => {
-    const regex = /^[a-zA-Z0-9]{6,}$/;
-    alias = alias.trim();
-    return regex.test(alias);
-};
-
-const validarColeccionFuentes = (fuentes) => {
-    return fuentes.length >= 2 !== false;
-};
